2a47c213bcc9312a9b99914f405f46fe96973b6b
Rocky Singh <rockygsingh@icloud.com>
2019-09-28 16:17:57 -0400

    update

---
+++ src/ToDoList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+
+class TodoList extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { todos: [] }
+    }
+    render() {
+        return (
+            <div>
+                <h1>Todo List</h1>
+                <ul>
+
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default TodoList;
