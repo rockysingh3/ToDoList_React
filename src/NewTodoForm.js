5f81e622716ea13483b843a1670df0739b1ff667
Rocky Singh <rockygsingh@icloud.com>
2019-09-29 18:39:30 -0400

    gets input from user

---
+++ src/NewTodoForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import uuid from 'uuid/v4'
+
+
+class NewTodoForm extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { task: "" }
+        this.hangleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        })
+    }
+
+    handleSubmit(evt) {
+        evt.preventDefault();
+        this.props.createTodo(this.state);
+        // we reset the state so all the info in the form is gone
+        this.setState({ task: "" })
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="task">New Todo</label> <br/>
+                    <input 
+                        type="text" 
+                        placeholder="New Todo"
+                        id="task"
+                        name="task"
+                        value={this.state.task}
+                        onChange={this.hangleChange} />
+                    <button>Add Todo</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default NewTodoForm;
